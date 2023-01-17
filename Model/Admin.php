fd95ec2d924c1729d76f8b97e96e61e0c33d603c
hugo-domage <hugo.domage-cicchelero@etu.univ-amu.fr>
2023-01-17 08:51:11 +0100

    Admin class model

---
+++ Model/Admin.php
@@ -0,0 +1,50 @@
+<?php
+
+class Admin{
+
+    /**
+     * @param $A_postParams
+     * @return array
+     */
+    public static function create(array $A_postParams):array{
+        $B_receive = Model::create($A_postParams, "admin");
+        if($B_receive){
+            return array('message' => "Utilisateur crée", 'status' => true);
+        }
+        return array('message' => "Utilisateur non crée", 'status' => false);
+    }
+
+    /**
+     * @param $A_postParams
+     * @return array
+     */
+    public static function delete(int $I_id):array{
+        $B_receive = Model::deleteById($I_id, "admin");
+        if($B_receive){
+            return array('message' => "Utilisateur supprimé", 'status' => true);
+        }
+        return array('message' => "Erreur de supression", 'status' => false);
+    }
+
+    /**
+     * @param $A_postParams
+     * @return array
+     */
+    public static function update(array $A_postParams):array{
+        $B_receive = Model::update($A_postParams, "admin");
+        if($B_receive){
+            return array('message' => "Changements enregistrés", 'status' => true);
+        }
+        return array('message' => "Erreur d'enregistrement", 'status' => false);
+    }
+
+    /**
+     * @param $A_postParams
+     * @return mixed
+     */
+    public static function selectById(int $I_id):array{
+        $A_receive = Model::selectById($I_id, "admin");
+        return $A_receive[0];
+    }
+
+}
