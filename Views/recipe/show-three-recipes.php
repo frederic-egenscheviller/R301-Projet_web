715762c7a3cde559d25b7d1bc2be91a753c9ec04
Frédéric Egenscheviller <frederic.egenscheviller@etu.univ-amu.fr>
2023-01-17 22:25:09 +0100

    Create home page with 3 random receipes

--- Views/recipe/show-three-recipes.php
+++ Views/recipe/show-three-recipes.php
@@ -1,10 +1,12 @@
 <?php
 
 echo '<link rel="stylesheet" type="text/css" href="../../static/styles/show-three-recipes.css">';
+echo '<h2>Faites de nouvelles découvertes culinaires !</h2>';
 echo '<div id="recipesContainer">';
 foreach ($A_view as $recipe){
     echo '<div class="recipe">';
-    echo $recipe['name'];
     echo "<img src='" . $recipe['picture'] . "'>";
+    echo "<p>".$recipe['name']."</p>";
+    echo '</div>';
 }
 echo '</div>';
