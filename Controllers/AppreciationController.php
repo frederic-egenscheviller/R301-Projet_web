afddb7ede8df28a7dc563d6b172c0b60b553f22f
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-01-17 08:40:24 +0100

    Add AppreciationController

---
+++ Controllers/AppreciationController.php
@@ -0,0 +1,29 @@
+<?php
+
+final class AppreciationController
+{
+    public function defaultAction($A_postParams) {
+        View::show("appreciation/show", Appreciation::selectById($A_postParams['id']));
+    }
+
+    public function createAction($A_postParams) {
+        View::show("status", Appreciation::create($A_postParams));
+    }
+
+    public function editAction($A_postParams) {
+        $this -> formAction();
+        View::show("appreciation/show", Appreciation::selectById($A_postParams['id']));
+    }
+
+    public function deleteAction($A_postParams) {
+        View::show("status", Appreciation::deleteById($A_postParams['id']));
+    }
+
+    public function formAction() {
+        View::show("appreciation/form");
+    }
+
+    public function updateAction($A_postParams) {
+        View::show("appreciation/form", Ustensil::update($A_postParams));
+    }
+}
