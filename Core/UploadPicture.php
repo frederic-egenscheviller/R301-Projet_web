6f2e551347d3a532559e6890c50bc99fae95ba63
Frédéric Egenscheviller <frederic.egenscheviller@etu.univ-amu.fr>
2023-01-17 09:46:19 +0100

    Add upload picture functionality for User and Receipe

---
+++ Core/UploadPicture.php
@@ -0,0 +1,32 @@
+<?php
+
+final class UploadPicture
+{
+    static function createPictureName(string $projectName): string
+    {
+        return strtolower(preg_replace("/[^A-Za-z]/", "", iconv('UTF-8', 'ASCII//TRANSLIT', $projectName)));
+    }
+
+    static function uploadPicture($elementName)
+    {
+        if (isset($_FILES['miniature'])) {
+            $elementName = self::createPictureName($elementName);
+
+            $extensions = ['jpg', 'jpeg'];
+            $maxSize = 5000000;
+
+            $tmpName = $_FILES['miniature']['tmp_name'];
+            $name = $_FILES['miniature']['name'];
+            $size = $_FILES['miniature']['size'];
+            $error = $_FILES['miniature']['error'];
+            $tabExtension = explode('.', $name);
+            $extension = strtolower(end($tabExtension));
+            if (in_array($extension, $extensions) && $size <= $maxSize && $error == 0) {
+                $file = $elementName . $extension;
+                move_uploaded_file($tmpName, "../content/" . $file);
+                return "../content/". $file;
+            }
+        }
+        return null;
+    }
+}
