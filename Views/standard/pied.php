6f0b3a0a4c26cef77db6e5af3d974c2e2162a38a
Ganassi <90609748+AlexandreGanassi@users.noreply.github.com>
2023-01-18 08:24:21 +0100

    Add files via upload

--- Views/standard/pied.php
+++ Views/standard/pied.php
@@ -1,18 +1,1 @@
-<link rel="stylesheet" type="text/css" href="footer.css">
-
-
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12 col-md-6">
-                <ul>
-                    <a href="#">Mentions Légales</a>
-                    <a href="#">Contact</a>
-                </ul>
-            </div>
-            <div class="col-sm-12 col-md-6">
-                <p>Copyright © <?php echo date("Y"); ?> Mon Site Web</p>
-            </div>
-        </div>
-    </div>
-</footer>
+<footer>Pied</footer>
