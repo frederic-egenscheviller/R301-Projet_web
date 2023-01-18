e1bbbff40709369b9186b542bdb07a6caeec0961
Mathieu Sauva <mathieu@MacBook-Air-de-Mathieu.local>
2023-01-18 17:29:18 +0100

    Update signin and signup

--- Views/signin/form.php
+++ Views/signin/form.php
@@ -1,12 +1,14 @@
 <?php
 echo("<form class='sigin' method='post' action='#'>
         <label><b>Nom d'utilisateur</b></label>
-        <input type='text' placeholder='votre pseudo' name='username' required><br>
+        <input type='text' placeholder='Votre pseudo' name='username' required><br>
 
         <label><b>Mot de passe</b></label>
         <input type='password' placeholder='Entrer le mot de passe' name='password' required>
 
-        <label><a href='./'><b>Mot de passe oublié?</b></label>
+       <label><a href='./'><b>Mot de passe oublié?</b></a></label>
 
         <input type='submit' id='submit' value='Se connecter' >
+        
+        <label><a href='/signup'><b>Créer un compte</b></a></label>
     </form>");
