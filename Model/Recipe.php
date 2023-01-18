660f7753ce4f031c83635f26f1a16d239949e610
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-01-18 20:26:25 +0100

    functions for the search bar

--- Model/Recipe.php
+++ Model/Recipe.php
@@ -2,9 +2,7 @@
 
 class Recipe extends Model
 {
-    /**
-     * @return array
-     */
+
     public static function randomRecipe():array{
         $O_con = Connection::initConnection();
         $S_sql = "SELECT * FROM RECIPE ORDER BY RANDOM() LIMIT 3";
@@ -13,7 +11,8 @@ class Recipe extends Model
         return $sth->fetchAll();
     }
 
-    public static function selectRecipeByUser(array $A_postParams):array{
+
+    static function selectRecipeByUser(array $A_postParams):array{
         $I_id = $A_postParams['id'];
         $O_con = Connection::initConnection();
         $S_sql = "SELECT * FROM RECIPE WHERE user_id = :user";
@@ -52,6 +51,43 @@ class Recipe extends Model
         $S_sql = "SELECT DISTINCT COST FROM RECIPE";
         $sth = $O_con->prepare($S_sql);
         $sth->execute();
+        return $sth->fetchAll();
+    }
+
+
+    public static function goodStringForSql(array $A_parms):string{
+        $S_goodString = "";
+        foreach($A_parms as $S_elem){
+            $S_goodString .= "'".$S_elem."'";
+        }
+        return substr($S_goodString,0,-2);
+    }
+
+
+    public static function searchRecipe(array $A_getParams):array{
+
+        $S_listIngredient = $A_getParams['ingredients'] ? self::goodStringForSql($A_getParams['ingredients']) : "select * from ingredients";
+        $S_listUtensil = $A_getParams['utensil'] ?  self::goodStringForSql($A_getParams['utensils']): "select * from utensils";
+        $S_listParticularity = $A_getParams['particularity'] ? self::goodStringForSql($A_getParams['particularities']) : "select * from particularities";
+        $S_search = $A_getParams['search'] ? $A_getParams['search'] : "";
+        
+        $S_sql = "SELECT distinct r.* 
+                    FROM Recipe r, Ingredients_recipe i, Utensils_recipe u, Particularities_recipe p
+                    WHERE r.name LIKE'%:search%'
+                    AND r.id = i.recipe_id
+                    AND i.ingredient_id IN (:listIngredient)
+                    AND r.id = u.recipe_id
+                    AND u.utensil_id IN (:listUstensil)
+                    AND r.id = p.recipe_id
+                    AND p.particularity_id IN (:listParticularity);";
+        
+        $O_con = Connection::initConnection();
+        $sth = $O_con->prepare($S_sql);
+        $sth->execute();
+        $sth->bindValue(':search', $S_search, PDO::PARAM_STR);
+        $sth->bindValue(':listIngredient', $S_listIngredient, PDO::PARAM_STR);
+        $sth->bindValue(':listUstensil', $S_listUtensil, PDO::PARAM_STR);
+        $sth->bindValue(':listParticularity', $S_listParticularity, PDO::PARAM_STR);
         return $sth->fetchAll();
     }
 
