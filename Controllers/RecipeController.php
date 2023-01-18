6ea9ba9d9dfe4526558b5d635037157131de8b1b
LennyGonzales <lenny.gonzales@etu.univ-amu.fr>
2023-01-18 10:13:12 +0100

    Change the defaultAction of RecipeController, UstensilController, ... to show all recipes, ingredients, ...

--- Controllers/RecipeController.php
+++ Controllers/RecipeController.php
@@ -2,7 +2,7 @@
 final class RecipeController
 {
     public function defaultAction($A_postParams) {
-        View::show("recipe/show", Recipe::selectById($A_postParams));
+        View::show("recipe/show", Recipe::select($A_postParams));
     }
     public function formAction() {
         View::show("recipe/form");
