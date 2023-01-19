5f895371c0c89392db80fa95502518dd77f015fb
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-01-20 00:44:46 +0100

    New filter-navbar to search on the homepage

---
+++ static/js/filterBtnShow.js
@@ -0,0 +1,30 @@
+document.getElementById("ingredientsBtn").addEventListener("click", function() {
+    toggleVisibility("dropIngredients");
+});
+document.getElementById("utensilsBtn").addEventListener("click", function() {
+    toggleVisibility("dropUtensils");
+});
+document.getElementById("cookingTimesBtn").addEventListener("click", function() {
+    toggleVisibility("dropTimes");
+});
+document.getElementById("cookingTypesBtn").addEventListener("click", function() {
+    toggleVisibility("dropTypes");
+});
+document.getElementById("difficultiesBtn").addEventListener("click", function() {
+    toggleVisibility("dropDifficulties");
+});
+document.getElementById("costsBtn").addEventListener("click", function() {
+    toggleVisibility("dropCosts");
+});
+document.getElementById("particularitiesBtn").addEventListener("click", function() {
+    toggleVisibility("dropParticularities");
+});
+
+function toggleVisibility(id) {
+    let element = document.getElementById(id);
+    if (element.style.visibility === "visible") {
+        element.style.visibility = "hidden";
+    } else {
+        element.style.visibility = "visible";
+    }
+}
