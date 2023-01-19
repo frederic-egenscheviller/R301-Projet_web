caefba627c5f7366dde10bc45a4588715190714b
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-01-19 17:05:48 +0100

    update function for search bar

--- Model/Recipe.php
+++ Model/Recipe.php
@@ -55,21 +55,17 @@ class Recipe extends Model
     }
 
 
-    public static function goodStringForSql(array $A_parms):string{
-        $S_goodString = "";
-        foreach($A_parms as $S_elem){
-            $S_goodString .= "'".$S_elem."'";
-        }
-        return substr($S_goodString,0,-2);
-    }
-
-
     public static function searchRecipe(array $A_getParams):array{
 
-        $S_listIngredient = $A_getParams['ingredients'] ? self::goodStringForSql($A_getParams['ingredients']) : "select * from ingredients";
-        $S_listUtensil = $A_getParams['utensil'] ?  self::goodStringForSql($A_getParams['utensils']): "select * from utensils";
-        $S_listParticularity = $A_getParams['particularity'] ? self::goodStringForSql($A_getParams['particularities']) : "select * from particularities";
-        $S_search = $A_getParams['search'] ? $A_getParams['search'] : "";
+        $S_listIngredient = isset($A_getParams['ingredients']) ? "'".implode("', '",$A_getParams['ingredients'])."'" : "select * from ingredients";
+        $S_listUtensil = isset($A_getParams['utensil']) ?  "'".implode("', '",$A_getParams['utensils'])."'": "select * from utensils";
+        $S_listParticularity = isset($A_getParams['particularity']) ? "'".implode("', '",$A_getParams['particularities'])."'" : "select * from particularities";
+
+        $S_search = isset($A_getParams['search']) ? $A_getParams['search'] : "";
+
+        $S_listDifficulty = isset($A_getParams['difficulty']) ? "'".implode("', '",$A_getParams['difficulty'])."'": "'facile','moyen','difficile'";
+        $S_listcost = isset($A_getParams['cost']) ? "'".implode("', '",$A_getParams['cost'])."'": "'Bon marché','Abordable','Coûteux'";
+        $I_cookingTime = isset($A_getParams['cookingTime']) ? intval($A_getParams['cookingTime']) : 1000000;
         
         $S_sql = "SELECT distinct r.* 
                     FROM Recipe r, Ingredients_recipe i, Utensils_recipe u, Particularities_recipe p
@@ -79,7 +75,10 @@ class Recipe extends Model
                     AND r.id = u.recipe_id
                     AND u.utensil_id IN (:listUstensil)
                     AND r.id = p.recipe_id
-                    AND p.particularity_id IN (:listParticularity);";
+                    AND p.particularity_id IN (:listParticularity)
+                    AND r.difficulty IN (:listDifficulty)
+                    AND r.cost  IN (:listcost)
+                    AND r.cooking_time < :cookingTime;";
         
         $O_con = Connection::initConnection();
         $sth = $O_con->prepare($S_sql);
@@ -88,6 +87,9 @@ class Recipe extends Model
         $sth->bindValue(':listIngredient', $S_listIngredient, PDO::PARAM_STR);
         $sth->bindValue(':listUstensil', $S_listUtensil, PDO::PARAM_STR);
         $sth->bindValue(':listParticularity', $S_listParticularity, PDO::PARAM_STR);
+        $sth->bindValue(':listDifficulty', $S_listDifficulty, PDO::PARAM_STR);
+        $sth->bindValue(':listcost', $S_listcost, PDO::PARAM_STR);
+        $sth->bindValue(':cookingTime', $I_cookingTime, PDO::PARAM_INT);
         return $sth->fetchAll();
     }
 
