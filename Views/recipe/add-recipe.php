bb72c212e739436392e958ec1b16812b5d1b20bd
LennyGonzales <lenny.gonzales@etu.univ-amu.fr>
2023-01-23 19:40:48 +0100

    Change the name in the add-recipe form and change its action

--- Views/recipe/add-recipe.php
+++ Views/recipe/add-recipe.php
@@ -1,9 +1,9 @@
 <?php
-echo "<form action='../helloworld/testform.php' method='POST'>
+echo "<form action='/addrecipe/update' method='POST'>
         <h1>Ajouter une recette</h1>
     
         <label><b>Nom de la recette</b></label>
-        <input type='text' placeholder='nom de la recette' name='recipename' required>
+        <input type='text' placeholder='nom de la recette' name='name' required>
         <br><br>
     
         <label><b>Photo de la recette</b></label>
@@ -22,23 +22,23 @@ echo "<form action='../helloworld/testform.php' method='POST'>
         <button type='button' value='addFields' onclick='addUtensil()'>Ajouter un ustensile</button><br><br>
 
         <label><b>Temps de preparation</b></label>
-        <input type='text' placeholder='temps de préparetion' name='preparationtime' required>
+        <input type='text' placeholder='temps de préparetion' name='cooking_time' required>
         <br><br>
 
         <label><b>Difficulte de la recette</b></label>
-        <input type='int' placeholder='difficulte de la recette' name='difficulté' required>
+        <input type='int' placeholder='difficulte de la recette' name='difficulty' required>
         <br><br>
 
         <label><b>Cout de la recette</b></label>
-        <input type='int' placeholder='cout de la recette' name='recipecost' required>
+        <input type='int' placeholder='cout de la recette' name='cost' required>
         <br><br>
 
         <label><b>Description de la recette</b></label>
-        <input type='text' placeholder='description de la recette' name='description' required>
+        <input type='text' placeholder='description de la recette' name='preparation_description' required>
         <br><br>
 
         <label><b>Type de cuisson</b></label>
-        <input type='text' placeholder='type de cuisson' name='cookingtype' required>
+        <input type='text' placeholder='type de cuisson' name='cooking_type' required>
         <br><br>
 
         <div id='container3'>
