06ccd7c411720b05d84fa8a6dc681b10fc4727b7
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-01-23 17:24:15 +0100

    update function selectBySearch

--- Model/Recipe.php
+++ Model/Recipe.php
@@ -60,41 +60,80 @@ class Recipe extends Model
 
 
     public static function searchRecipe(array $A_getParams):array{
-
-        $S_listIngredient = isset($A_getParams['ingredients']) ? "'".implode("', '",$A_getParams['ingredients'])."'" : "select * from ingredients";
-        $S_listUtensil = isset($A_getParams['utensil']) ?  "'".implode("', '",$A_getParams['utensils'])."'": "select * from utensils";
-        $S_listParticularity = isset($A_getParams['particularity']) ? "'".implode("', '",$A_getParams['particularities'])."'" : "select * from particularities";
-
-        $S_search = isset($A_getParams['search']) ? $A_getParams['search'] : "";
-
-        $S_listDifficulty = isset($A_getParams['difficulty']) ? "'".implode("', '",$A_getParams['difficulty'])."'": "'facile','moyen','difficile'";
-        $S_listcost = isset($A_getParams['cost']) ? "'".implode("', '",$A_getParams['cost'])."'": "'Bon marché','Abordable','Coûteux'";
-        $I_cookingTime = isset($A_getParams['cookingTime']) ? intval($A_getParams['cookingTime']) : 1000000;
-        
         $S_sql = "SELECT distinct r.* 
-                    FROM Recipe r, Ingredients_recipe i, Utensils_recipe u, Particularities_recipe p
-                    WHERE r.name LIKE'%:search%'
-                    AND r.id = i.recipe_id
-                    AND i.ingredient_id IN (:listIngredient)
-                    AND r.id = u.recipe_id
-                    AND u.utensil_id IN (:listUstensil)
-                    AND r.id = p.recipe_id
-                    AND p.particularity_id IN (:listParticularity)
-                    AND r.difficulty IN (:listDifficulty)
-                    AND r.cost  IN (:listcost)
-                    AND r.cooking_time < :cookingTime;";
-        
+        FROM Recipe r, Ingredients_recipe i, Utensils_recipe u, Particularities_recipe p
+        WHERE r.name LIKE :search ";
+
+        $S_search = isset($A_getParams['search']) ? "%".$A_getParams['search']."%" : "'%%'";
+
+        $A_params = array(':search'=>array($S_search,PDO::PARAM_STR));
+
+        function addToQuery($table,$A_getParams){
+            $S_underSql = "select id from $table
+            where ";
+            foreach($A_getParams as $value){
+                $S_underSql .= " id = :param$value or";
+                $A_params[':param'.$value]=array($value, PDO::PARAM_STR);
+            }
+            $S_underSql = substr( $S_underSql,0,-3);
+            return array($S_underSql, $A_params);
+        }
+
+        if(isset($A_getParams['ingredients'])){
+            $A_result = addToQuery("ingredients",$A_getParams['ingredients']);
+            $A_params += $A_result[1];
+            $S_sql.="AND r.id = i.recipe_id
+            AND i.ingredient_id IN (". $A_result[0].") ";
+        }
+
+        if(isset($A_getParams['utensil'])){
+            $A_result = addToQuery("utensils",$A_getParams['utensils']);
+            $A_params += $A_result[1];
+            $S_sql.="AND r.id = u.recipe_id
+            AND u.utensil_id IN (". $A_result[0].") ";
+        }
+
+        if(isset($A_getParams['particularity'])){
+            $A_result = addToQuery("particularities",$A_getParams['particularities']);
+            $A_params += $A_result[1];
+            $S_sql.="AND r.id = u.recipe_id
+            AND u.utensil_id IN (". $A_result[0].") ";
+        }
+
+        if(isset($A_getParams['cooking_times']) && intval($A_getParams['cooking_times'])>1){
+            $I_cookingTime = intval($A_getParams['cooking_times']);
+            $S_sql.="AND r.cooking_time <= :cookingTime ";
+            $A_params[':cookingTime']=array($I_cookingTime,PDO::PARAM_INT);
+        }
+
+        if(isset($A_getParams['difficulties'])){
+            $S_sql.="AND (";
+            foreach($A_getParams['difficulties'] as $value){
+                $S_sql .= "r.difficulty = :param$value or ";
+                $A_params[':param'.$value]=array($value, PDO::PARAM_STR);
+            }
+            $S_sql = substr( $S_sql,0,-3);
+            $S_sql.=") ";
+        }
+
+        if(isset($A_getParams['costs'])){
+            $S_sql.="AND (";
+            foreach($A_getParams['costs'] as $value){
+                $S_sql .= "r.cost = :param$value or ";
+                $A_params[':param'.$value]=array($value, PDO::PARAM_STR);
+            }
+            $S_sql = substr( $S_sql,0,-3);
+            $S_sql.=") ";
+        }
         $O_con = Connection::initConnection();
         $sth = $O_con->prepare($S_sql);
+
+        foreach($A_params as $key => $value){
+            $sth->bindValue($key,  $value[0], $value[1]);
+        }
+
         $sth->execute();
-        $sth->bindValue(':search', $S_search, PDO::PARAM_STR);
-        $sth->bindValue(':listIngredient', $S_listIngredient, PDO::PARAM_STR);
-        $sth->bindValue(':listUstensil', $S_listUtensil, PDO::PARAM_STR);
-        $sth->bindValue(':listParticularity', $S_listParticularity, PDO::PARAM_STR);
-        $sth->bindValue(':listDifficulty', $S_listDifficulty, PDO::PARAM_STR);
-        $sth->bindValue(':listcost', $S_listcost, PDO::PARAM_STR);
-        $sth->bindValue(':cookingTime', $I_cookingTime, PDO::PARAM_INT);
-        return $sth->fetchAll();
+        return($sth->fetchAll());
     }
 
 }
