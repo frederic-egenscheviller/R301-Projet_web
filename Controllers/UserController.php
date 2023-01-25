5ef127d3bce4ef515964be9e6842574be9889407
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-01-25 18:22:34 +0100

    Add connexion, add recipe and disconnexion buttons

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -6,21 +6,9 @@ final class UserController
         View::show("errors/error404");
     }
 
-    public function createAction($A_postParams) {
-        $A_postParams = Users::uploadPictures($A_postParams);
-        View::show("status", Users::create($A_postParams));
-    }
-
-    public function deleteAction($A_postParams) {
-        View::show("status", Users::deleteById($A_postParams['id']));
-    }
-
-    public function updateAction($A_postParams) {
-        $A_postParams = Users::uploadPictures($A_postParams);
-        View::show("status", Users::update($A_postParams));
-    }
-
-    public function connectionAction($A_postParams) {
-        View::show("status", Users::connection($A_postParams));
+    public function logoutAction() {
+        Session::destroy();
+        header('Location: /home');
+        exit;
     }
 }
--- Views/standard/header.php
+++ Views/standard/header.php
@@ -1,17 +1,34 @@
+<?php
+
+echo '
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <section class="top-nav">
     <div>
         JuJu KitKat
     </div>
     <input id="menu-toggle" type="checkbox" />
-    <label class='menu-button-container' for="menu-toggle">
-        <div class='menu-button'></div>
+    <label class="menu-button-container" for="menu-toggle">
+        <div class="menu-button"></div>
     </label>
     <ul class="menu">
         <li><a href="#">Accueil</a></li>
         <li><a href="#">Recettes</a></li>
         <li><a href="#">Compte</a></li>
         <li><a href="#">A propos</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="#">Contact</a></li>';
+if (Session::check()) {
+    if(Session::getSession()['status'] == 'admin') {
+        echo '
+        <li><a href="/addrecipe">Ajouter une recette</a></li>
+        <li><a href="/user/logout">Déconnexion</a></li>';
+    } else if(Session::getSession()['status'] == 'user') {
+        echo '
+        <li><a href="/user/logout">Déconnexion</a></li>';
+    }
+} else {
+    echo '
+        <li><a href="/signin">Connexion</a></li>';
+}
+echo '
     </ul>
-</section>
+</section>';
