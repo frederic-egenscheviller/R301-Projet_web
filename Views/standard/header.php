d420ecec9ed73780b9c2a9420cc3e69850899548
LennyGonzales <lenny.gonzales@etu.univ-amu.fr>
2023-01-25 21:46:10 +0100

    fix the header

--- Views/standard/header.php
+++ Views/standard/header.php
@@ -1,24 +1,23 @@
 <?php
 
-echo '
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<section class="top-nav">
+echo "
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<section class='top-nav'>
     <div>
         JuJu KitKat
     </div>
-    <input id="menu-toggle" type="checkbox" />
-    <label class="menu-button-container" for="menu-toggle">
-        <div class="menu-button"></div>
+    <input id='menu-toggle' type='checkbox' />
+    <label class='menu-button-container'' for='menu-toggle'>
+        <div class='menu-button'></div>
     </label>
-    <ul class="menu">
-        <li><a href="/home">Accueil</a></li>
-        <li><a href="/allrecipes">Recettes</a></li>
-        <li><a href="/profil">Compte</a></li>
-        <li><a href="/contact/form">Contact</a></li>
+    <ul class='menu'>
+        <li><a href='/home'>Accueil</a></li>
+        <li><a href='/allrecipes'>Recettes</a></li>
+        <li><a href='/profil'>Compte</a></li>
+        <li><a href='/contact/form'>Contact</a></li>";
 if (Session::check()) {
     if(Session::getSession()['status'] == 'admin') {
-        echo '
-        <li><a href="/addrecipe">Ajouter une recette</a></li>';
+        echo '<li><a href="/addrecipe">Ajouter une recette</a></li>';
     }
     echo '<li><a href="/logout">Déconnexion</a></li>';
 } else {
