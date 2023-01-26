68bdbc8a5ef1dcc200c959ef5ef989f40ef14fd2
Ceccarelli Luca <luca.ceccarelli@etu.univ-amu.fr>
2023-01-26 01:04:05 +0100

    Added the possibility to delete an appreciation

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -7,7 +7,9 @@ class AdminController
             header("Location: /home");
             return;
         }
-        View::show("/admin/admin-form");
+        View::show("/admin/add-admin-form");
+        View::show("/admin/delete-user-form");
+        View::show("admin/delete-appreciation", Appreciation::selectAll());
     }
 
     public function deleteAction(Array $A_parametres = null, Array $A_postParams = null){
@@ -21,6 +23,11 @@ class AdminController
         if(Users::checkIfExistsById($A_postParams["id"])){
             Admin::create($A_postParams);
         }
+        header("Location: /admin");
+    }
+
+    public function updateAction(Array $A_parametres = null, Array $A_postParams = null){
+        Appreciation::deleteByID($A_parametres[0]);
         header("Location: /admin");
     }
 }
